fullcontact/full.bootstrap

promoted to 0.5.0-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.4.24-SNAPSHOT"
+(defproject fullcontact/full.bootstrap "0.5.0-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,15 +10,15 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fullcontact/full.core "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.time "0.4.24-SNAPSHOT"]
-                 [fullcontact/camelsnake "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.json "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.async "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.dev "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.cache "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.metrics "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.http "0.4.24-SNAPSHOT"]]
+                 [fullcontact/full.core "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.time "0.5.0-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.json "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.async "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.dev "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.cache "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.http "0.5.0-SNAPSHOT"]]
 
   :aot :all
 

fullcontact/full.metrics

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.metrics "0.4.24-SNAPSHOT"
+(defproject fullcontact/full.metrics "0.5.0-SNAPSHOT"
   :description "Clojure application metrics sugar for Riemann backend."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -12,8 +12,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [riemann-clojure-client "0.2.11"]
                  [com.climate/clj-newrelic "0.2.1"]
-                 [fullcontact/full.async "0.4.24-SNAPSHOT"]
-                 [fullcontact/full.core "0.4.24-SNAPSHOT"]]
+                 [fullcontact/full.async "0.5.0-SNAPSHOT"]
+                 [fullcontact/full.core "0.5.0-SNAPSHOT"]]
 
   :aot :all
 