fullcontact/full.rabbit

Version 0.4.1-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rabbit "0.4.0"
+(defproject fullcontact/full.rabbit "0.4.1-SNAPSHOT"
   :description "RabbitMQ sugar on top of langohr."
 
   :dependencies [[com.novemberain/langohr "3.0.0-rc2"]

fullcontact/full.metrics

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.metrics "0.4.0"
+(defproject fullcontact/full.metrics "0.4.1-SNAPSHOT"
   :description "Clojure application metrics sugar for Riemann backend."
 
   :dependencies [[riemann-clojure-client "0.2.11"]