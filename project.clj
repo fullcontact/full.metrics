fullcontact/full.bootstrap

Version 0.7.4-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.7.3"
+(defproject fullcontact/full.bootstrap "0.7.4-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,15 +10,15 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [fullcontact/full.core "0.7.3"]
-                 [fullcontact/full.time "0.7.3"]
-                 [fullcontact/camelsnake "0.7.3"]
-                 [fullcontact/full.json "0.7.3"]
-                 [fullcontact/full.async "0.7.3"]
-                 [fullcontact/full.dev "0.7.3"]
-                 [fullcontact/full.cache "0.7.3"]
-                 [fullcontact/full.metrics "0.7.3"]
-                 [fullcontact/full.http "0.7.3"]]
+                 [fullcontact/full.core "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.time "0.7.4-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.json "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.async "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.dev "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.cache "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.http "0.7.4-SNAPSHOT"]]
 
   :aot :all
 

fullcontact/full.metrics

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.metrics "0.7.3"
+(defproject fullcontact/full.metrics "0.7.4-SNAPSHOT"
   :description "Clojure application metrics sugar for Riemann backend."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -13,8 +13,8 @@
                  [riemann-clojure-client "0.2.11" :exclusions [org.clojure/tools.logging
                                                                org.slf4j/slf4j-api]]
                  [com.climate/clj-newrelic "0.2.1"]
-                 [fullcontact/full.async "0.7.3"]
-                 [fullcontact/full.core "0.7.3"]]
+                 [fullcontact/full.async "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.core "0.7.4-SNAPSHOT"]]
 
   :aot :all
 