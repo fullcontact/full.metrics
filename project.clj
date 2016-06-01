fullcontact/full.bootstrap

Version 0.8.19-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.8.18"
+(defproject fullcontact/full.bootstrap "0.8.19-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,15 +10,15 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [fullcontact/full.core "0.8.18"]
-                 [fullcontact/full.time "0.8.18"]
-                 [fullcontact/camelsnake "0.8.18"]
-                 [fullcontact/full.json "0.8.18"]
-                 [fullcontact/full.async "0.8.18"]
-                 [fullcontact/full.dev "0.8.18"]
-                 [fullcontact/full.cache "0.8.18"]
-                 [fullcontact/full.metrics "0.8.18"]
-                 [fullcontact/full.http "0.8.18"]]
+                 [fullcontact/full.core "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.time "0.8.19-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.json "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.async "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.dev "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.cache "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.http "0.8.19-SNAPSHOT"]]
 
   :aot :all
 

fullcontact/full.metrics

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.metrics "0.8.18"
+(defproject fullcontact/full.metrics "0.8.19-SNAPSHOT"
   :description "Clojure application metrics sugar for Riemann backend."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -13,8 +13,8 @@
                  [riemann-clojure-client "0.4.1" :exclusions [org.clojure/tools.logging
                                                                org.slf4j/slf4j-api]]
                  [com.climate/clj-newrelic "0.2.1"]
-                 [fullcontact/full.async "0.8.18"]
-                 [fullcontact/full.core "0.8.18"]]
+                 [fullcontact/full.async "0.8.19-SNAPSHOT"]
+                 [fullcontact/full.core "0.8.19-SNAPSHOT"]]
   :aot [full.metrics] ;clojure new relic extension doesn't work when aot'ed.
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})