df858f97a713681a22c09b790452490bf891f02d
Karlis Lauva <k@karlis.me>
2016-06-21 15:05:19 -0600

    upgrade to core 0.10

--- project.clj
+++ project.clj
@@ -5,12 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [riemann-clojure-client "0.4.1" :exclusions [org.clojure/tools.logging
                                                                org.slf4j/slf4j-api]]
                  [com.climate/clj-newrelic "0.2.1"]
                  [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.9.0"]]
+                 [fullcontact/full.core "0.10.0"]]
   :aot [full.metrics]  ; clojure new relic extension doesn't work when AOT'ed.
   :plugins [[lein-midje "3.1.3"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
