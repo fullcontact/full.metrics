a7be0136455ad00c3d106a66f987bc624216122f
jeremyplichtafc <45575699+jeremyplichtafc@users.noreply.github.com>
2020-02-03 10:16:58 -0700

    Jplichta/clojure 1.10 (#2)
    * fix travis and project.clj versions
    
    * use newer full.core

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject fullcontact/full.metrics "0.11.5-SNAPSHOT"
+(defproject fullcontact/full.metrics "0.12.0"
   :description "Clojure application metrics and monitoring sugar"
   :url "https://github.com/fullcontact/full.metrics"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [riemann-clojure-client "0.4.1" :exclusions [org.clojure/tools.logging
                                                               org.slf4j/slf4j-api
                                                               com.google.protobuf/protobuf-java]]
                  [com.google.protobuf/protobuf-java "3.1.0"]
                  [clj-statsd "0.4.0"]
                  [com.climate/clj-newrelic "0.2.1"]
-                 [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.10.2" :exclusions [org.clojure/clojurescript]]]
+                 [fullcontact/full.async "1.1.0"]
+                 [fullcontact/full.core "1.1.1" :exclusions [org.clojure/clojurescript]]]
   :aot [full.metrics]  ; clojure new relic extension doesn't work when AOT'ed.
-  :plugins [[lein-midje "3.1.3"]]
+  :plugins [[lein-midje "3.2.2"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -24,4 +24,4 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]}})
