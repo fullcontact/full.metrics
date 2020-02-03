7c0532e007e053dd75f24c91c7c8cc7230299aa8
Jeremy Plichta <jeremy.plichta@fullcontact.com>
2020-02-03 10:15:49 -0700

    use newer full.core

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [clj-statsd "0.4.0"]
                  [com.climate/clj-newrelic "0.2.1"]
                  [fullcontact/full.async "1.1.0"]
-                 [fullcontact/full.core "1.1.0" :exclusions [org.clojure/clojurescript]]]
+                 [fullcontact/full.core "1.1.1" :exclusions [org.clojure/clojurescript]]]
   :aot [full.metrics]  ; clojure new relic extension doesn't work when AOT'ed.
   :plugins [[lein-midje "3.2.2"]]
   :release-tasks [["vcs" "assert-committed"]
